2ded0ecf02370028b2e667f6e5c3fd402f75903c
yann <yann.boisselier@gmail.com>
2018-07-13 18:48:31 +0200

    fix lint issues

--- plugins/hydrate-layout-data.js
+++ plugins/hydrate-layout-data.js
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import { getPosts } from '~/api/api'
+// import axios from 'axios'
+// import { getPosts } from '~/api/api'
 
 /**
  * We do this to achieve server side rendering for
