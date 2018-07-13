d9dff826b26d19b4d43ccfcf1cebb9b5f544d838
yann <yann.boisselier@gmail.com>
2018-07-13 18:57:21 +0200

    fix deprecated warning

--- plugins/app.js
+++ plugins/app.js
@@ -2,7 +2,7 @@
  * App logic that not belongs to any specific component
  */
 export default (context) => {
-  if (context.isClient) {
+  if (process.client) {
     // we want menu mobile to close when changing page.
     window.onNuxtReady((app) => {
       app.$nuxt.$on('routeChanged', (to, from) => {
